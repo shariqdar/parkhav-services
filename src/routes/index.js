865d67c88f59c66d5499b687a53f3adbd4e4ca56
shariq dar <dshariq8@gmail.com>
2020-11-28 01:02:48 +0530

    testing git setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'Welcome to Express API template' });
+  return res.status(200).json({ message: 'Welcome to Express API template2s' });
 });
 module.exports = router;
