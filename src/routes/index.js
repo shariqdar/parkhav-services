e5e865a5986e875662ef8889955e97ffb49dcab0
shariq dar <dshariq8@gmail.com>
2020-11-29 23:24:25 +0530

    setup project commit #3

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../settings';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', (req, res) =>
-  res.status(200).json({message: 'Welcome to express API'})
-);
+  res.status(200).json({ message: testEnvironmentVariable }));
 
 export default indexRouter;
