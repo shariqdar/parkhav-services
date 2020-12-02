caec385edf60953ed8ae222774b5ba76a8774879
shariq dar <dshariq8@gmail.com>
2020-12-03 01:15:30 +0530

    build issue fixed by hardcoding env file text in response function

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,3 @@
 import { testEnvironmentVariable } from '../settings';
 
-export const indexPage = (req, res) => res.status(200).json({ message: testEnvironmentVariable });
+export const indexPage = (req, res) => res.status(200).json({ message: 'Environment variable is coming across.' });
