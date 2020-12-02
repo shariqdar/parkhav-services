a188387e8c1cae84acf86efe2942570b03f68d2e
shariq dar <dshariq8@gmail.com>
2020-12-03 01:06:54 +0530

    build failing commit #3

--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,9 @@ describe('Index page test', () => {
   it('gets base url', done => {
     server
       .get(`${BASE_URL}/`)
-      .expect(201)
+      .expect(200)
       .end((err, res) => {
-        expect(res.status).to.equal(200);
+        expect(res.status).to.equal(200);console.log("hello",res.body.message,res.status)
         expect(res.body.message).to.equal(
           'Environment variable is coming across.'
         );
