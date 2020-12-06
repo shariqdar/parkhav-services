dfd4f0bd8a6688f75b20f87f0f44b18ecd7c68ae
shariq dar <dshariq8@gmail.com>
2020-12-06 20:11:57 +0530

    adding post request

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { indexPage, messagesPage } from '../controllers';
+import { indexPage, messagesPage, addMessage } from '../controllers';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 
 //indexRouter.get('/messages', (req, res) => res.status(200).json({message:"hello"}))
 indexRouter.get('/messages', messagesPage)
+
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
