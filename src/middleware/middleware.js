4dd6f8c78c72d44e17923593218bb491e8a8b8cd
shariq dar <dshariq8@gmail.com>
2020-12-09 00:29:01 +0530

    minor issue

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,10 +1,12 @@
+import axios from 'axios';
+
 export const modifyMessage = (req, res, next) => {
     req.body.message = `SAYS: ${req.body.message}`;
     next();
   };
 
   export const performAsyncAction = async (req, res, next) => {
-    try {
+    try {console.log(req.body.message)
       await axios.get('https://picsum.photos/id/0/info');
       next();
     } catch (err) {
