7f7c1efc82fcf694f6edc7666806ec2b2863ed22
shariq dar <dshariq8@gmail.com>
2020-12-10 01:00:39 +0530

    change database url string name

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config();
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
-export const connectionString = process.env.CONNECTION_STRING;
+export const connectionString = process.env.DATABASE_URL;
